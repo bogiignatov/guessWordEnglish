4a1e872de762a608cea05ff57b4d0804a88ce334
bogiig <119950688+bogiignatov@users.noreply.github.com>
2023-02-03 17:23:52 +0200

    Make a project

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # guessWordEnglish
+
+"Guess the word" is a game in which you know a predefined word. The words are preset and one is chosen at random. When starting the game, information about the number of failed attempts that can be made is displayed. The word to be guessed, the number of letters in boxes. There is also a help box that gives a brief description of the search term. Pressing the "Reset game" button restarts the game. When a word is successfully known, it says "Congrats! You found the word: (word)." If the word is not known, "Game over! You don't have remaining guesses! The word is: (word)" is displayed.
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+"Познай думата" е игра в която се познава предварително зададена дума. Думите са предварително зададени и се избира една на произволен принцип. При стартиране на играта се показва информация за броят неуспешни опити, които може да се направят. Думата, която трябва да се познае, броят на буквите в кутийки. Има и поле за помощ, което дава кратко описание за търсената дума. При натискане на бутона "Reset game" играта се рестартира. При успешно позната дума се изписва "Congrats! You found the word: (дума)." Ако думата не е позната се показва "Game over! You don't have remaining guesses! The word is: (дума)"
+
+---------------------------------------------------------------------------------------------------------------------------------------------
+
+# Live Demo
+
+You can play the game directly in your Web browser here:
+
+[<img alt="Play Button" src="https://github.com/bogiignatov/guessWordEnglish/blob/main/image.jpg />](https://replit.com/@bogiignatov/guessTheWord#index.html)
+
+Може да играете играта директно през вашия Уеб браузър тук:
+
+[<img alt="Play Button" src="https://github.com/bogiignatov/guessWordEnglish/blob/main/image.jpg />](https://replit.com/@bogiignatov/guessTheWord#index.html)
---
+++ words.js
@@ -0,0 +1,198 @@
+const wordList = [
+  {
+    word: "apple",
+    hint: "А red fruit growing on a tree",
+  },
+  {
+    word: "ocean",
+    hint: "A huge body of water with boats",
+  },
+  {
+    word: "television",
+    hint: "Immediate reproduction of image and sound",
+  },
+  {
+    word: "glass",
+    hint: "Thin material that is placed on doors, windows",
+  },
+  {
+    word: "gold",
+    hint: "A precious metal with a yellow color and a strong luster",
+  },
+  {
+    word: "silver",
+    hint: "A white and lustrous precious metal that can be made into jewelry",
+  },
+  {
+    word: "cocktail",
+    hint: "A mixture of several types of alcoholic drinks with sugar and fruit",
+  },
+  {
+    word: "pencil",
+    hint: "A tool with a hole and a knife for sharpening pencils",
+  },
+  {
+    word: "bench",
+    hint: "A device for seating several people, usually a wooden table on stilts",
+  },
+  {
+    word: "car",
+    hint: "A vehicle with four wheels",
+  },
+  {
+    word: "motorcycle",
+    hint: "A two-wheeled motor vehicle",
+  },
+  {
+    word: "bicycle",
+    hint: "A two-wheeled vehicle that is driven by the feet",
+  },
+  {
+    word: "plant",
+    hint: "A non-motile organism that feeds on the soil by means of roots, stems and leaves",
+  },
+  {
+    word: "map",
+    hint: "Drawing of the earth's surface, on which geographical and geological latitudes are given",
+  },
+  {
+    word: "island",
+    hint: "A piece of land that is surrounded on all sides by water",
+  },
+  {
+    word: "calendar",
+    hint: "A book or sheet with marked days and months of a year",
+  },
+  {
+    word: "carton",
+    hint: "A box used for packing objects and goods",
+  },
+  {
+    word: "programmer",
+    hint: "A person who makes programs for electronic machines",
+  },
+  {
+    word: "facebook",
+    hint: "A large and famous social network",
+  },
+  {
+    word: "yacht",
+    hint: "A small luxury cruise ship",
+  },
+  {
+    word: "person",
+    hint: "A living being that is capable of thinking and speaking and leads a social life",
+  },
+  {
+    word: "pillow",
+    hint: "A canvas bag that serves as a pillow for the head when sleeping",
+  },
+  {
+    word: "processor",
+    hint: "A device that performs basic computing operations",
+  },
+  {
+    word: "compass",
+    hint: "A device that shows the directions of the world and is used for orientation",
+  },
+  {
+    word: "sun",
+    hint: "A celestial body that illuminates and warms the earth during the day",
+  },
+  {
+    word: "rain",
+    hint: "Water drops that fall from the clouds",
+  },
+  {
+    word: "cloud",
+    hint: "Accumulation of water vapor in the atmosphere",
+  },
+  {
+    word: "bridge",
+    hint: "A facility for crossing a river or chasm",
+  },
+  {
+    word: "hospital",
+    hint: "A facility that treats people",
+  },
+  {
+    word: "hippopotamus",
+    hint: "A huge South African mammal with a large head and thick skin",
+  },
+  {
+    word: "crocodile",
+    hint: "A large amphibian predatory lizard-like reptile",
+  },
+  {
+    word: "lightning",
+    hint: "An electrical discharge in the atmosphere accompanied by loud thunder",
+  },
+  {
+    word: "blood",
+    hint: "A red liquid that circulates in the body",
+  },
+  {
+    word: "courier",
+    hint: "A person who delivers business papers as well as mail",
+  },
+  {
+    word: "cake",
+    hint: "A round confection made of several layers of dough and cream in between",
+  },
+  {
+    word: "trolleybus",
+    hint: "A multi-seater vehicle in urban transport that moves with electricity on a trackless road",
+  },
+  {
+    word: "tram",
+    hint: "A city vehicle that consists of one or more wagons and moves on a rail track with electricity",
+  },
+  {
+    word: "subway",
+    hint: "Urban underground or overhead electric railway line",
+  },
+  {
+    word: "key",
+    hint: "A small metal device with a special shape for unlocking and locking a door",
+  },
+  {
+    word: "thunder",
+    hint: "A loud or prolonged crash that accompanies lightning during a thunderstorm",
+  },
+  {
+    word: "umbrella",
+    hint: "A hemispherical object made of stretched canvas to protect against rain",
+  },
+  {
+    word: "truck",
+    hint: "Large cargo vehicle",
+  },
+  {
+    word: "plane",
+    hint: "A vehicle with wings and an engine for carrying people and cargo",
+  },
+  {
+    word: "elevator",
+    hint: "Lifting equipment for automatic ascent and lowering of people and loads",
+  },
+  {
+    word: "kitchen",
+    hint: "A room in a dwelling intended for eating",
+  },
+  {
+    word: "dubai",
+    hint: "Famous desert vacation destination",
+  },
+  {
+    word: "photo",
+    hint: "Photographic image",
+  },
+  {
+    word: "pet",
+    hint: "An animal we watch at home",
+  },
+  {
+    word: "translator",
+    hint: "A person who translates from one language to another",
+  },
+];
